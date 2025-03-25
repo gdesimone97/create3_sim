a0c8d61d25958b55beff0289167dc7f589a01db9
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2025-03-26 00:45:16 +0100

    Update control.py

--- irobot_create_common/irobot_create_control/launch/include/control.py
+++ irobot_create_common/irobot_create_control/launch/include/control.py
@@ -6,9 +6,9 @@
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, RegisterEventHandler
-from launch.conditions import LaunchConfigurationNotEquals
+from launch.conditions import IfCondition
 from launch.event_handlers import OnProcessExit
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration, NotEqualsSubstitution, PathJoinSubstitution
 from launch_ros.actions import Node
 
 ARGUMENTS = [
@@ -30,12 +30,16 @@ def generate_launch_description():
         executable='spawner',
         namespace=namespace,  # Namespace is not pushed when used in EventHandler
         parameters=[control_params_file],
-        arguments=arguments=[
+        arguments=[
             'diffdrive_controller',
             '-c',
             'controller_manager',
             '--controller-manager-timeout',
-            '30'
+            '60',
+            '--service-call-timeout',
+            '60',
+            '--switch-timeout',
+            '60',
         ],
         output='screen',
     )
@@ -48,7 +52,11 @@ def generate_launch_description():
             '-c',
             'controller_manager',
             '--controller-manager-timeout',
-            '30'
+            '60',
+            '--service-call-timeout',
+            '60',
+            '--switch-timeout',
+            '60',
         ],
         output='screen',
     )
@@ -75,7 +83,7 @@ def generate_launch_description():
             ('/tf_static', 'tf_static')
         ],
         output='screen',
-        condition=LaunchConfigurationNotEquals('namespace', '')
+        condition=IfCondition(NotEqualsSubstitution(LaunchConfiguration('namespace'), ''))
     )
 
     # Static transform from <namespace>/base_link to base_link
@@ -91,7 +99,7 @@ def generate_launch_description():
             ('/tf_static', 'tf_static')
         ],
         output='screen',
-        condition=LaunchConfigurationNotEquals('namespace', '')
+        condition=IfCondition(NotEqualsSubstitution(LaunchConfiguration('namespace'), ''))
     )
 
     ld = LaunchDescription(ARGUMENTS)
